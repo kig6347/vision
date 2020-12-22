852e033de03c3d212d93b4b5314f412764911168
kig6347 <59467704+kig6347@users.noreply.github.com>
2020-12-23 00:29:33 +0900

    Create mouse_click_dot_while.py
    while 사용
    마우스 이벤트

---
+++ project/mouse_click_dot_while.py
@@ -0,0 +1,20 @@
+import numpy as np
+import cv2
+
+# 마우스 이벤트 콜백 발생 시 함수 실행
+def mouse_callback(event, x, y, flags, param):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        cv2.circle(img, (x, y), 10, (255, 0, 0), -1) # 도형함수
+        print("키눌림")
+
+img = np.zeros((512,512,3), np.uint8)
+# 마우스 이벤트 콜백 받을 윈도우 창
+cv2.namedWindow('image')
+# image 원도우에서 마우스 이벤트가 발생하면 함수 호출
+cv2.setMouseCallback('image', mouse_callback)
+while(1):
+  cv.imshow('image',img)
+  key=cv.waitKey(1)
+  if key ==27:
+    break
+cv2.destroyAllWindows()
