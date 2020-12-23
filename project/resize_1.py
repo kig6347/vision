ec6982b9aa6c2a9c7e222fe1d908a5d38aed58ad
kig6347 <59467704+kig6347@users.noreply.github.com>
2020-12-23 14:55:18 +0900

    사이즈 변경(cv.resize)

---
+++ project/resize_1.py
@@ -0,0 +1,14 @@
+import cv2 as cv
+img= cv.imread('lenna.png', cv.IMREAD_COLOR)
+if img is None:
+  print("이미지 파일을 읽을 수 없습니다.")
+  exit(1)
+res1 = cv.resize(img,None,fx=0.5, fy=0.5, interpolation = cv.INTER_CUBIC)
+#OR
+height, width = img.shape[:2]
+res2 = cv.resize(img,(height*2, width*2), interpolation = cv.INTER_CUBIC)
+
+cv.imshow("img",img)
+cv.imshow("res1",res1)
+cv.imshow("res2",res2)
+cv.waitKey(0)
