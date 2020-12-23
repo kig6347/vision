1f032820d6aa8f9d5466e05d53f22ec13b474d0e
kig6347 <59467704+kig6347@users.noreply.github.com>
2020-12-23 14:34:24 +0900

    동영상 열기

---
+++ project/video.py
@@ -0,0 +1,21 @@
+import cv2
+#cap = cv2.VideoCapture(0) # 0: default camera
+cap = cv2.VideoCapture("Seoul.mp4") #
+동영상 파일에서 읽기
+while cap.isOpened():
+  #
+  카메라 프레임 읽기
+  success, frame = cap.read()
+  if success:
+    #프레임 출력
+    cv2.imshow('Camera Window', frame)
+    # ESC를 누르면 종료
+    key = cv2.waitKey(10) & 0xFF
+    if (key == 27):
+      break
+  else :
+    break
+    
+    
+cap.release()
+cv2.destroyAllWindows()
