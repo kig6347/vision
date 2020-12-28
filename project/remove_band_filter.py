0711c4a857f042a52299f4311ab0a92c232195f0
kig6347 <59467704+kig6347@users.noreply.github.com>
2020-12-28 17:38:18 +0900

    밴드 제거 필터

---
+++ project/remove_band_filter.py
@@ -0,0 +1,41 @@
+import cv2
+import numpy as np
+
+def displayDFT(name,src):
+  src1 = src[:,:,0];
+  src2 = src[:, :, 1];
+  mag_image = cv2.magnitude(src1,src2)
+  mag_image += 1
+  spectrum = np.log(np.abs(mag_image))
+  dft = cv2.normalize(spectrum,None,0,1,cv2.NORM_MINMAX)
+  cv2.imshow(name,dft)
+  
+img = cv2.imread('lunar.png',cv2.IMREAD_GRAYSCALE)
+rows, cols = img.shape
+crow,ccol = int(rows/2), int(cols/2)
+
+bandFilter = np.zeros((rows,cols,2),np.float32)
+for i in range(rows):
+  for j in range(cols):
+    if(j>(ccol   - 10) and j < (ccol + 10) and i > (crow + 10) ):
+      bandFilter[i,j] = 0
+    elif (j>(ccol  -10) and j < (ccol + 10) and i < (crow  -10)):
+      bandFilter[i, j] = 0
+    else:
+      bandFilter[i, j] = 1
+      
+dft = cv2.dft(np.float32(img),flags = cv2.DFT_COMPLEX_OUTPUT)
+dft_shift = np.fft.fftshift(dft)
+
+fshift = cv2.multiply(dft_shift,bandFilter)
+f_ishift = np.fft.ifftshift(fshift)
+img_back = cv2.idft(f_ishift)
+img_back = cv2.magnitude(img_back[:,:,0],img_back[:,:,0])
+idft = cv2.normalize(np.abs(img_back),None,0,1,cv2.NORM_MINMAX)
+
+displayDFT("DFT2",dft_shift)
+displayDFT("DFT",fshift)
+
+cv2.imshow("org",img)
+cv2.imshow("inverted",idft)
+cv2.waitKey(0)
